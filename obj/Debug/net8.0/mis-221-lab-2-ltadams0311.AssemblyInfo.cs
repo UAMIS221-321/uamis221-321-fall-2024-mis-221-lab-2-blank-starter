349870f3f73c17ec4bdad96537bb23f798951c7c
ltadams0311 <ltadams@crimson.ua.edu>
2024-09-08 22:11:31 -0500

    Hello

--- obj/Debug/net8.0/mis-221-lab-2-ltadams0311.AssemblyInfo.cs
+++ obj/Debug/net8.0/mis-221-lab-2-ltadams0311.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mis-221-lab-2-ltadams0311")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6beb5fcdfcf434119a0b4b5205353d107cac576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad07f0636d198c2d89ce8956c135c393ef52baf")]
 [assembly: System.Reflection.AssemblyProductAttribute("mis-221-lab-2-ltadams0311")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mis-221-lab-2-ltadams0311")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
